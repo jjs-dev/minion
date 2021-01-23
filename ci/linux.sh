b7847796709e7912509dc2046d65333a4eb15ef0
Mikail Bagishov <bagishov.mikail@yandex.ru>
2021-01-23 21:23:55 +0300

    install deps

--- ci/linux.sh
+++ ci/linux.sh
@@ -10,7 +10,8 @@ echo "::group::Preparing"
 if [[ $CI_CGROUPS == "cgroup-v2" ]] && [ -z "${CI_VM+set}" ]; then
   echo "::group::Preparing virtual machine"
   sudo apt-get update
-  #sudo apt-get install -y virtualbox-5.2
+  sudo apt-get install -y libopus0 libqt5core5a libqt5gui5 libqt5opengl5 libqt5printsupport5 \
+      libqt5widgets5 libqt5x11extras5 libsdl1.2debian libvpx5
   wget https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb -q -O vagrant.deb
   wget https://download.virtualbox.org/virtualbox/5.2.44/virtualbox-5.2_5.2.44-139111~Ubuntu~bionic_amd64.deb -q -O vb.deb
   sudo dpkg -i vb.deb
