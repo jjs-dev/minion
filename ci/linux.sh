ddaf39f4f9926e70cea0cfb6ef1328b7e8a7e9d5
Mikail Bagishov <bagishov.mikail@yandex.ru>
2021-01-23 21:27:45 +0300

    install libvpx manually

--- ci/linux.sh
+++ ci/linux.sh
@@ -11,9 +11,11 @@ if [[ $CI_CGROUPS == "cgroup-v2" ]] && [ -z "${CI_VM+set}" ]; then
   echo "::group::Preparing virtual machine"
   sudo apt-get update
   sudo apt-get install -y libopus0 libqt5core5a libqt5gui5 libqt5opengl5 libqt5printsupport5 \
-      libqt5widgets5 libqt5x11extras5 libsdl1.2debian libvpx5
+      libqt5widgets5 libqt5x11extras5 libsdl1.2debian
   wget https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb -q -O vagrant.deb
   wget https://download.virtualbox.org/virtualbox/5.2.44/virtualbox-5.2_5.2.44-139111~Ubuntu~bionic_amd64.deb -q -O vb.deb
+  http://security.ubuntu.com/ubuntu/pool/main/libv/libvpx/libvpx5_1.7.0-3ubuntu0.18.04.1_amd64.deb -q -O libvpx.deb
+  sudo dkpg -i libvpx.deb
   sudo dpkg -i vb.deb
   sudo dpkg -i vagrant.deb
   vagrant --version
