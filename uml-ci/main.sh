6c27678ffc912c24e94e4294d0079048fcf3e6ea
Sergey Lisov <sleirsgoevy@gmail.com>
2021-01-30 15:16:09 +0300

    brevno

--- uml-ci/main.sh
+++ uml-ci/main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 wget -q https://download.fedoraproject.org/pub/fedora/linux/releases/33/Cloud/x86_64/images/Fedora-Cloud-Base-33-1.2.x86_64.raw.xz -O img.xz
 unxz img.xz
 wget -q https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.93.tar.xz
@@ -10,9 +12,6 @@ export ARCH=um
 make defconfig
 make -j3
 )
-linux-5.4.93/linux mem=4096M ubda=img rootfstype=hostfs init=/bin/
-linux-5.4.93/linux mem=4096M ubda=img rootfstype=hostfs init=/bin/true
-linux-5.4.93/linux mem=4096M ubda=img rootfstype=hostfs init=/bin/bash "$PWD"/uml-setup.sh
-#linux-5.4.93/linux mem=4096M ubda=img rootfstype=hostfs init="$PWD"/uml-setup.sh
-#linux-5.4.93/linux mem=4096M ubda=img root=/dev/ubda1 rootfstype=ext4 hostfs=.. eth0=slirp,,./uml-slirp.sh &
-#./uml-ssh.sh
+linux-5.4.93/linux mem=4096M ubda=img rootfstype=hostfs init="$PWD"/uml-setup.sh
+linux-5.4.93/linux mem=4096M ubda=img root=/dev/ubda1 rootfstype=ext4 hostfs=.. eth0=slirp,,./uml-slirp.sh &
+./uml-ssh.sh
