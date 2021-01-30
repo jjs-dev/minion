5b320bfb8e5f751ed6ebd0b58b676721231186fb
Sergey Lisov <sleirsgoevy@gmail.com>
2021-01-30 14:09:45 +0300

    [WIP] Run cgroup v2 tests using UML

---
+++ uml-ci/payload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo dnf install -y gcc gcc-c++ strace zip
+curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain stable
+export CI_OS=fedora
+export CI_CGROUPS=cgroup-v2
+export CI_TARGET=x86_64-unknown-linux-musl
+export CI_VM=1
+pwd
+find
+cat ci/linux.sh
---
+++ uml-ci/uml-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir /dev/mnt
+mount /dev/ubda1 /dev/mnt
+sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /dev/mnt/etc/ssh/sshd_config
+echo >> /dev/mnt/usr/lib/systemd/system/rc-local.service
+echo '[Install]' >> /dev/mnt/usr/lib/systemd/system/rc-local.service
+echo 'WantedBy=multi-user.target' >> /dev/mnt/usr/lib/systemd/system/rc-local.service
+sed -i 's/After=.*/After=sshd.service/' /dev/mnt/usr/lib/systemd/system/rc-local.service
+chroot /dev/mnt systemctl enable rc-local
+echo '#!/bin/bash
+
+echo root:root | chpasswd
+ifconfig eth0 10.0.2.15 netmask 255.255.255.0
+route add default dev eth0
+echo nameserver 10.0.2.3 > /etc/resolv.conf
+exit 0
+' > /dev/mnt/etc/rc.d/rc.local
+chmod +x /dev/mnt/etc/rc.d/rc.local
+umount /dev/mnt
+sync
+poweroff -f
