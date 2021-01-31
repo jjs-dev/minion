8d23ded4de69b4bf0f2643e03dbe819591d9336b
Sergey Lisov <sleirsgoevy@gmail.com>
2021-01-31 12:38:41 +0300

    logs.zip -> &#78008;

--- ci/linux.sh
+++ ci/linux.sh
@@ -16,7 +16,7 @@ if [[ $CI_CGROUPS == "cgroup-v2" ]] && [ -z "${CI_VM+set}" ]; then
   #sudo sshpass -p user ssh -p 2224 user@127.0.0.1 "cat /vagrant/logs.zip | base64" | base64 --decode > logs.zip # WTF??
   sudo sshpass -p user ssh -p 2224 user@127.0.0.1 sudo poweroff || true
   rm stracez-dummy
-  unzip logs.zip
+  #unzip logs.zip
   sleep 10
   echo "Current directory after VM finish"
   ls .
@@ -36,5 +36,5 @@ sudo --preserve-env ./tests/$CI_TARGET/minion-tests --trace
 echo "::group::Finalize"
 echo "Current directory after running tests"
 ls .
-echo "Collecting logs to archive"
-zip logs.zip strace*
+#echo "Collecting logs to archive"
+#zip logs.zip strace*
