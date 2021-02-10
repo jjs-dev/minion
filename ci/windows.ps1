74345fcb789559064559233a686cb812d3654914
Mikail Bagishov <bagishov.mikail@yandex.ru>
2021-02-10 19:02:25 +0300

    does ampersand help&

--- ci/windows.ps1
+++ ci/windows.ps1
@@ -5,7 +5,7 @@ Write-Output "Target: $env:CI_TARGET"
 Write-Output "::group::Running tests"
 
 $env:RUST_BACKTRACE = "full"
-"./tests/$env:CI_TARGET/minion-tests.exe" --trace
+& "./tests/$env:CI_TARGET/minion-tests.exe" --trace
 if ($LASTEXITCODE -ne 0) {
     throw "tests failed"
 }
